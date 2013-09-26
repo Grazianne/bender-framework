70b76534e5bfed827e20f65170d49e2870f948e8
Jefersson Nathan <admin@phpse.net>
2013-09-26 11:45:26 -0300

    Use Api to create Query Consulting and use DOMDocument
    - Remove string with query, and use the DataAccess of Joomla! for more compatibilities in future and has more sense.
    - Use DOMDocument to make html more fast and correctly.

--- framework/class/render.php
+++ framework/class/render.php
@@ -3,30 +3,63 @@ class benderRender
 {
 	public function getPositions()
 	{
-		$sql = 'SELECT * FROM `#__bender_positions` ORDER BY ordering ASC';
 		$db = JFactory::getDbo();
+		$sql = $db->getQuery(true);
+		
+		$sql->select('*')
+			->from('#__bender_positions')
+			->order('ordering ASC');
+			
 		$db->setQuery($sql);
+		
 		return $db->loadObjectList();
 	}
 	
 	public function renderPositions()
 	{
-		$html = '';
+		$html = new DOMDocument;
 		foreach($this->getPositions() as $value)
 		{
-			$html .= '<div class="row" data-name="'.$value->name.'">
-						<div class="position">';
-						if($value->name != 'component')
-						{
-							$html .= '<p><span class="remove">
-							<img src="'. JURI::base()  .'templates/bender/framework/img/trash.png" />
-							</span></p>';
-						}
-			$html .= $value->name;
-			$html .= '</div></div>';
+			// Create a main row
+			$divRow = $html->createElement('div');
+			$divRow->setAttribute('class', 'row');
+			$divRow->setAttribute('data-name', $value->name);
+			
+			//div position
+			$divPosition = $html->createElement('div');
+			$divPosition->setAttribute('class', 'position');
+			
+			if($value->name != 'component')
+			{
+				// Image to delete position
+				$image = $html->createElement('img');
+				$image->setAttribute('src', JURI::base() . 'templates/bender/framework/img/trash.png');
+				
+				$span = $html->createElement('span');
+				$span->setAttribute('class', 'remove');
+				
+				$p = $html->createElement('p');
+				
+				$nameOfPosition = $html->createElement('p', $value->name);
+				
+				// Set Positions
+				$span->appendChild($image);
+				$p->appendChild($span);
+				$p->appendChild($nameOfPosition);
+				$divPosition->appendChild($p);
+			}
+			else
+			{
+				$p = $html->createElement('p', 'Display Component');
+				$divPosition->appendChild($p);
+			}
+			
+			$divRow->appendChild($divPosition);
+			$html->appendChild($divRow);
+			
 			
 		}
-		return $html;
+		return $html->saveHTML();
 	}
 	
 }
